647c7045cbc12ecb43344cc31af45161e5d304d8
luciaparuntu <prntulucia246@gmail.com>
2021-10-06 10:33:53 +0700

    first commit

---
+++ tugaspbo2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main (void)
+
+{
+ int tahun, umur;
+ char lagi;
+ 
+ mulai :
+  cout << "\nMasukan tahun kelahiranmu = ";
+  cin >> tahun;
+  umur = 2021 - tahun;
+  cout << "Umurrmu " << umur << " tahun\n";
+return 0;
